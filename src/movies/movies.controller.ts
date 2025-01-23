4e4b61709fcccbd976217e27a684e78b81196755
Maja <90241095+MajciRock@users.noreply.github.com>
2025-01-23 18:55:00 +0100

    Prenos profesorjevega dela
    Prenos dela, ki so ga v šoli delali 17.1

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie';
+import { CreateMovieDto } from './entities/create-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  async findAll(): Promise<Movie[]> {
+    return this.movieService.findAll();
+  }
+
+  @Post()
+  async create(@Body() createMovieDto: CreateMovieDto): Promise<Movie> {
+    return this.movieService.create(createMovieDto);
+  }
+}
