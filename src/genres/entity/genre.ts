f7a2f5a58c8da07322473f61a63516ca399f2520
Maja <90241095+MajciRock@users.noreply.github.com>
2025-01-24 12:17:21 +0100

    dokončan genres

--- src/genres/entity/genre.ts
+++ src/genres/entity/genre.ts
@@ -2,6 +2,8 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('genres')
 export class Genre {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   title: string;
 }
