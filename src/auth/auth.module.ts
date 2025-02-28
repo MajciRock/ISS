3f388088f0c6e5883e74925b3d8f3861adff4c74
Maja <90241095+MajciRock@users.noreply.github.com>
2025-02-28 10:06:07 +0100

    profesorjova koda nadoknadit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'jkjaskdspka',
+      signOptions: { expiresIn: '5h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
